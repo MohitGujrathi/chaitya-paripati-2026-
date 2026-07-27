79623bcccdb4dfe916918d8ecb0cc0e9896a2d7e
MohitGujrathi <mohit_guju@yahoo.co.in>
2026-07-27 15:46:50 +0530

    Add mobile search interface

--- app/main.py
+++ app/main.py
@@ -1,59 +1,82 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
+
 import pandas as pd
 from pathlib import Path
 
-app = FastAPI(
-    title="Passenger Search API",
-    version="1.0"
+
+app = FastAPI()
+
+
+templates = Jinja2Templates(
+    directory="app/templates"
 )
+
+
+app.mount(
+    "/static",
+    StaticFiles(directory="app/static"),
+    name="static"
+)
+
 
 EXCEL_FILE = Path("data/passengers.xlsx")
 
 
+
 @app.get("/")
-def home():
-    return {
-        "message": "Passenger Search API is running"
-    }
+def home(request: Request):
 
+    return templates.TemplateResponse(
+        "index.html",
+        {
+            "request": request
+        }
+    )
 
-@app.get("/passengers")
-def get_passengers():
-    df = pd.read_excel(EXCEL_FILE)
-
-    return df.fillna("").to_dict(orient="records")
 
 
 @app.get("/search")
-def search_passenger(
-    name: str | None = Query(None),
-    mobile: str | None = Query(None),
-    pnr: str | None = Query(None)
+def search(
+    name: str = "",
+    mobile: str = "",
+    pnr: str = ""
 ):
 
     df = pd.read_excel(EXCEL_FILE)
 
+
     result = df
 
+
     if name:
+
         result = result[
             result["Name"]
             .astype(str)
             .str.contains(name, case=False, na=False)
         ]
 
+
     if mobile:
+
         result = result[
             result["Mobile"]
             .astype(str)
             .str.contains(mobile, na=False)
         ]
 
+
     if pnr:
+
         result = result[
             result["PNR"]
             .astype(str)
             .str.contains(pnr, na=False)
         ]
 
-    return result.fillna("").to_dict(orient="records")
+
+    return result.fillna("").to_dict(
+        orient="records"
+    )
