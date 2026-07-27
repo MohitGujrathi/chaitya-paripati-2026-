931fb4c157fdbc197b10de4ca6f2e882e651a2d0
MohitGujrathi <mohit_guju@yahoo.co.in>
2026-07-27 14:23:22 +0530

    Updated code

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+import pandas as pd
+from pathlib import Path
 
 app = FastAPI(
-    title="Passenger Search App",
+    title="Passenger Search API",
     version="1.0"
 )
+
+EXCEL_FILE = Path("data/passengers.xlsx")
 
 
 @app.get("/")
@@ -13,8 +17,43 @@ def home():
     }
 
 
-@app.get("/health")
-def health_check():
-    return {
-        "status": "OK"
-    }
+@app.get("/passengers")
+def get_passengers():
+    df = pd.read_excel(EXCEL_FILE)
+
+    return df.fillna("").to_dict(orient="records")
+
+
+@app.get("/search")
+def search_passenger(
+    name: str | None = Query(None),
+    mobile: str | None = Query(None),
+    pnr: str | None = Query(None)
+):
+
+    df = pd.read_excel(EXCEL_FILE)
+
+    result = df
+
+    if name:
+        result = result[
+            result["Name"]
+            .astype(str)
+            .str.contains(name, case=False, na=False)
+        ]
+
+    if mobile:
+        result = result[
+            result["Mobile"]
+            .astype(str)
+            .str.contains(mobile, na=False)
+        ]
+
+    if pnr:
+        result = result[
+            result["PNR"]
+            .astype(str)
+            .str.contains(pnr, na=False)
+        ]
+
+    return result.fillna("").to_dict(orient="records")
